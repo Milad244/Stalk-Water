0abede52f8e8447e9d9e8e6c70f0ff094c02b4e8
praisej04 <jakazapraise@gmail.com>
2025-09-27 17:11:35 -0700

    changed main

--- Backend/user.cpp
+++ Backend/user.cpp
@@ -17,6 +17,7 @@ public:
     //Constructors
     User();
     User(const string&, const double&, const string&, const double&, const int&);
+    User (const string&, const double&, const string&);
     //Destructor
     ~User();
     //Getters
@@ -38,7 +39,7 @@ public:
     friend istream& operator>>(istream&, User&);
     friend ostream& operator<<(ostream&, const User&);
     void addDrink(const string& date, double ml);
-    void drinkMore(User u); //returns how much water need to drink more
+    int drinkMore()const; //returns how much water need to drink more
 
 
 };
@@ -51,6 +52,7 @@ struct WaterLog
 
 User::User() : name("N/A"), weight(0), gender("N/A"), waterLevel(0.0), drinksPerDay(0) {}
 User::User(const string& n, const double& w, const string& g, const double& wl, const int& d) : name(n), weight(w), gender(g), waterLevel(wl), drinksPerDay(d) {}
+User::User (const string& n, const double& w, const string& g):  name(n), weight(w), gender(g){};
 User::~User() {}
 
 //Getters
@@ -140,24 +142,24 @@ void User::addDrink(const string &date, double ml)
         cout << "Log is full! Cannot add more than 100 entries." << endl;
     }
 }
-void drinkMore(User u)
+
+int User::drinkMore()const
 {
-    int print = u.getWeight() * 30 - u.getWaterLevel();
-    cout << "You need to drink more" << print << "ml to be healthier !" << endl;
+    return static_cast<int>(getWeight() * 30); // returns ml
 }
 
+int main(int argc, char* argv[]) {
+    string name = argc > 1 ? argv[1] : "Jane Doe";
+    double weight = argc > 2 ? stod(argv[2]) : 0;
+    string gender = argc > 3 ? argv[3] : "N/A";
+    double waterLevel = argc > 4 ? stod(argv[4]) : 0.0;
+    int drinksPerDay = argc > 5 ? stoi(argv[5]) : 0;
+    User user(name, weight, gender, waterLevel, drinksPerDay); // existing user
+    User user (name, weight, gender); // new user
 
-int main() {
-    User u1;
-    cout << "Enter an User";
-    cin >> u1;
+    // output JSON to node.js
+    user.printInfo(cout);
 
-    drinkMore(u1);
-
-    u1.~User();
-
-    system("Pause");
     return 0;
-
     
 }
